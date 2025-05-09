0e2ea082de6c743d85e62019a29902a5a67b4641
LatencyIndex <latency.index@gmail.com>
2025-05-09 02:43:49 +0200

    Add NamedOrderBook

--- src/market/data.rs
+++ src/market/data.rs
@@ -1,5 +1,7 @@
 //! Elemental market datatypes, not specific to any exchange.
 
+use serde::Serialize;
+use std::cmp::{Eq, Ord, Ordering, PartialEq, PartialOrd};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -12,7 +14,7 @@ pub enum ParseError {
     SerdeError(#[from] serde_json::Error),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub struct Order {
     pub price: f64,
     pub amount: f64,
@@ -41,4 +43,110 @@ impl TryFrom<&Vec<String>> for Order {
 pub struct OrderBook {
     pub asks: Vec<Order>,
     pub bids: Vec<Order>,
+}
+
+#[derive(Serialize)]
+pub struct NamedBid {
+    pub price: f64,
+    pub amount: f64,
+    pub exchange: String,
+}
+
+impl NamedBid {
+    pub fn new(exchange: String, order: Order) -> Self {
+        Self {
+            price: order.price,
+            amount: order.amount,
+            exchange,
+        }
+    }
+}
+
+impl PartialEq for NamedBid {
+    fn eq(&self, other: &Self) -> bool {
+        self.cmp(other).is_eq()
+    }
+}
+
+impl Eq for NamedBid {}
+
+impl PartialOrd for NamedBid {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+/// The better bid (greater price, then greater amount) compares greater.
+impl Ord for NamedBid {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.price
+            .total_cmp(&other.price)
+            .then(self.amount.total_cmp(&other.amount))
+    }
+}
+
+#[derive(Serialize)]
+pub struct NamedAsk {
+    pub price: f64,
+    pub amount: f64,
+    pub exchange: String,
+}
+
+impl NamedAsk {
+    pub fn new(exchange: String, order: Order) -> Self {
+        Self {
+            price: order.price,
+            amount: order.amount,
+            exchange,
+        }
+    }
+}
+
+impl PartialEq for NamedAsk {
+    fn eq(&self, other: &Self) -> bool {
+        self.cmp(other).is_eq()
+    }
+}
+
+impl Eq for NamedAsk {}
+
+impl PartialOrd for NamedAsk {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+/// The better ask (lower price, then greater amount) compares greater.
+impl Ord for NamedAsk {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.price
+            .total_cmp(&other.price)
+            .reverse()
+            .then(self.amount.total_cmp(&other.amount))
+    }
+}
+
+#[derive(Serialize)]
+pub struct NamedOrderBook {
+    pub spread: f64,
+    pub asks: Vec<NamedAsk>,
+    pub bids: Vec<NamedBid>,
+}
+
+impl NamedOrderBook {
+    pub fn new(exchange: &str, book: &OrderBook) -> Self {
+        Self {
+            spread: 0.0, // TODO
+            asks: book
+                .asks
+                .iter()
+                .map(|&ask| NamedAsk::new(exchange.to_string(), ask))
+                .collect(),
+            bids: book
+                .bids
+                .iter()
+                .map(|&bid| NamedBid::new(exchange.to_string(), bid))
+                .collect(),
+        }
+    }
 }
