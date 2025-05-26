af577ff9e7218418b5fc8c9e12f199ffd39c0e0f
LatencyIndex <latency.index@gmail.com>
2025-05-26 07:29:46 +0200

    Remove Config Default impl

--- src/bin/sample_config.rs
+++ src/bin/sample_config.rs
@@ -1,5 +1,33 @@
+use rusty_markets::{
+    market::{data::OrderBookFormat, ExchangeConfig},
+    network::websocket::DurableWSConfig,
+    Config,
+};
+use tokio::time::Duration;
+
 fn main() {
-    let config = rusty_markets::Config::default();
+    let config = Config {
+        exchanges: vec![
+            ExchangeConfig {
+                label: "binance".to_string(),
+                url: "wss://stream.binance.com:9443/ws/ethbtc@depth20@100ms".to_string(),
+                inits: Vec::new(),
+                order_book_format: OrderBookFormat::Binance,
+            },
+            ExchangeConfig {
+                label: "bitstamp".to_string(),
+                url: "wss://ws.bitstamp.net".to_string(),
+                inits: vec![
+                    r#"{"event":"bts:subscribe","data":{"channel":"order_book_ethbtc"}}"#
+                        .to_string(),
+                ],
+                order_book_format: OrderBookFormat::Bitstamp,
+            },
+        ],
+        connection: DurableWSConfig::default(),
+        order_lifetime: Duration::from_secs(8),
+        nb_orders: 10,
+    };
     let json = serde_json::to_string_pretty(&config).unwrap();
     println!("{json}");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,7 @@ pub mod algorithm;
 pub mod market;
 pub mod network;
 
-use crate::{
-    market::{data::OrderBookFormat, ExchangeConfig},
-    network::websocket::DurableWSConfig,
-};
+use crate::{market::ExchangeConfig, network::websocket::DurableWSConfig};
 use serde::{Deserialize, Serialize};
 use tokio::time::Duration;
 
@@ -15,31 +12,4 @@ pub struct Config {
     pub connection: DurableWSConfig,
     pub order_lifetime: Duration,
     pub nb_orders: usize,
-}
-
-impl Default for Config {
-    fn default() -> Self {
-        Self {
-            exchanges: vec![
-                ExchangeConfig {
-                    label: "binance".to_string(),
-                    url: "wss://stream.binance.com:9443/ws/ethbtc@depth20@100ms".to_string(),
-                    inits: Vec::new(),
-                    order_book_format: OrderBookFormat::Binance,
-                },
-                ExchangeConfig {
-                    label: "bitstamp".to_string(),
-                    url: "wss://ws.bitstamp.net".to_string(),
-                    inits: vec![
-                        r#"{"event":"bts:subscribe","data":{"channel":"order_book_ethbtc"}}"#
-                            .to_string(),
-                    ],
-                    order_book_format: OrderBookFormat::Bitstamp,
-                },
-            ],
-            connection: DurableWSConfig::default(),
-            order_lifetime: Duration::from_secs(8),
-            nb_orders: 10,
-        }
-    }
 }
